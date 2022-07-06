a676e722ff6ede968f27ce1addf5cacdcf19c1a6
Kirankumar Ambati <kiran.chinna12520@gmail.com>
2022-07-06 20:27:11 +0530

    fix: code improvements for Back component

--- src/components/Back/index.tsx
+++ src/components/Back/index.tsx
@@ -8,10 +8,11 @@ const Back = (props: BackProps) => {
     const {
         heading,
         color,
-        textStyle = { ...typographyGuide.back.heading },
+        textStyle = typographyGuide.back.heading,
         rightElement,
         onClick,
     } = props;
+    const { white } = mainColors;
 
     return (
         <Row className="h-center" style={{ padding: '12px 0px' }}>
@@ -20,31 +21,33 @@ const Back = (props: BackProps) => {
                     <svg width="32" height="12" viewBox="0 0 32 12" fill="none">
                         <path
                             d="M31 6L0.999999 6"
-                            stroke={color ?? mainColors.white}
+                            stroke={color ?? white}
                             strokeWidth="1.5"
                             strokeLinecap="square"
                             strokeLinejoin="round"
                         />
                         <path
                             d="M7 12C7 8.68629 4.31371 6 1 6"
-                            stroke={color ?? mainColors.white}
+                            stroke={color ?? white}
                             strokeWidth="1.5"
                         />
                         <path
                             d="M1 6C4.31371 6 7 3.31371 7 0"
-                            stroke={color ?? mainColors.white}
+                            stroke={color ?? white}
                             strokeWidth="1.5"
                         />
                     </svg>
                 </Row>
                 <VerticalSpacer n={4} />
                 {heading ? (
-                    <Typography {...textStyle} color={color ?? mainColors.white}>
+                    <Typography {...textStyle} color={color ?? white}>
                         {heading}
                     </Typography>
                 ) : null}
             </Row>
-            <div style={{ marginLeft: 'auto' }}>{rightElement ?? null}</div>
+            <div style={{ marginLeft: 'auto' }}>
+                {rightElement}
+            </div>
         </Row>
     );
 };
