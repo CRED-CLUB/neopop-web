3c075f2a0070dd8367b08bdf83267d048108ddf5
maxim <fedaraumaxim@gmail.com>
2022-07-08 20:28:51 +0300

    Created wrapper for Storybok spinner. Created it to place spinner in Stroybook docs and playground in center.

---
+++ src/components/Spinner/components/StorybookWrapper.component.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StorybookWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+`;
+
+export default StorybookWrapper;
